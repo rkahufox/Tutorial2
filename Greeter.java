1d7d4b5efaac069d91ef38817a165c78b6003aee
Robert <robert.tk.fox@gmail.com>
2018-08-09 01:44:09 +0000

    Removed main function from Greeter.java

---
+++ Greeter.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Greeter {
+
+  private Locale locale;
+  private String name;
+
+  public Greeter(String language, String country, String name) {
+    locale = new Locale(language, country);
+    this.name = name;
+  }
+
+  public String sayHello() {
+    ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+    return messages.getString("greeting") + ", " +"_"+name+"_";
+  }
+
+  
+}
